48d0f6a0ff0b4ae16daac96f075ba513c11be70c
antonellanicolino <antonellanicolino1999@gmail.com>
2026-05-06 23:41:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const img = document.querySelector(".character");
+
+function blink() {
+
+  img.src = "images/Specky2.png";
+
+  setTimeout(() => {
+    img.src = "images/Specky1.png";
+    setTimeout(blink, Math.random() * 4000 + 2000);
+  }, 120);
+}
+
+blink();
+
+
+
+const textBox = document.getElementById("dialogue");
+
+document.querySelector(".head").onclick = () => setNextText("head");
+document.querySelector(".torso").onclick = () => setNextText("torso");
+document.querySelector(".lower").onclick = () => setNextText("lower");
+
+function setNextText(type) {
+  const lines = messages[type];  
+  const i = index[type];        
+
+  textBox.textContent = lines[i];
+
+  index[type] = (i + 1) % lines.length;
+}
+
+const messages = {
+
+  head: [
+    "You're looking for meaning.",
+    "Think a bit harder.",
+    "Why are you here?",
+    "",
+  ],
+
+  torso: [
+    "I'm hungry.",
+    "Don't poke me.",
+    "Hihihi, that tickles."
+  ],
+
+  lower: [
+    "Hey... buy me dinner first.",
+    "That's inappropriate!",
+    "Don't be a weirdo now.",
+    "Nope."
+  ]
+};
+
+const index = {
+  head: 0,
+  torso: 0,
+  lower: 0
+};
