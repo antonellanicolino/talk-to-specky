0f72be4dfe0886708349d3785394e7449be410ba
antonellanicolino <antonellanicolino1999@gmail.com>
2026-05-07 01:56:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-window.addEventListener("DOMContentLoaded", () => {
+const img = document.querySelector(".character");
 
-  const img = document.querySelector(".character");
-  const textBox = document.getElementById("dialogue");
+function blink() {
 
-  const head = document.querySelector(".head");
-  const torso = document.querySelector(".torso");
-  const lower = document.querySelector(".lower");
+  img.src = "images/Specky2.png";
 
-  const messages = {
-    head: [
-      "You're looking for meaning.",
-      "Think a bit harder.",
-      "Why are you here?",
-      "",
-    ],
-    torso: [
-      "I'm hungry.",
-      "Don't poke me.",
-      "Hihihi, that tickles."
-    ],
-    lower: [
-      "Hey... buy me dinner first.",
-      "That's inappropriate!",
-      "Don't be a weirdo now.",
-      "Nope."
-    ]
-  };
+  setTimeout(() => {
+    img.src = "images/Specky1.png";
+    setTimeout(blink, Math.random() * 4000 + 2000);
+  }, 120);
+}
 
-  const index = {
-    head: 0,
-    torso: 0,
-    lower: 0
-  };
+blink();
 
-  function setNextText(type) {
-    const lines = messages[type];
-    const i = index[type];
 
-    textBox.textContent = lines[i];
-    index[type] = (i + 1) % lines.length;
-  }
 
-  head.onclick = () => setNextText("head");
-  torso.onclick = () => setNextText("torso");
-  lower.onclick = () => setNextText("lower");
+const textBox = document.getElementById("dialogue");
 
-  function blink() {
-    img.src = "images/Specky2.png";
+document.querySelector(".head").onclick = () => setNextText("head");
+document.querySelector(".torso").onclick = () => setNextText("torso");
+document.querySelector(".lower").onclick = () => setNextText("lower");
 
-    setTimeout(() => {
-      img.src = "images/Specky1.png";
-      setTimeout(blink, Math.random() * 4000 + 2000);
-    }, 120);
-  }
+function setNextText(type) {
+  const lines = messages[type];  
+  const i = index[type];        
 
-  blink();
+  textBox.textContent = lines[i];
 
+  index[type] = (i + 1) % lines.length;
+}
+
+const messages = {
+
+  head: [
+    "You're looking for meaning.",
+    "Think a bit harder.",
+    "Why are you here?",
+    "",
+  ],
+
+  torso: [
+    "I'm hungry.",
+    "Don't poke me.",
+    "Hihihi, that tickles."
+  ],
+
+  lower: [
+    "Hey... buy me dinner first.",
+    "That's inappropriate!",
+    "Don't be a weirdo now.",
+    "Nope."
+  ]
+};
+
+const index = {
+  head: 0,
+  torso: 0,
+  lower: 0
 });
