83b2c63407edb155187e7b2afd2dc7c9d8124432
antonellanicolino <antonellanicolino1999@gmail.com>
2026-05-07 01:45:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-const img = document.querySelector(".character");
+window.addEventListener("DOMContentLoaded", () => {
 
-function blink() {
+  const img = document.querySelector(".character");
+  const textBox = document.getElementById("dialogue");
 
-  img.src = "images/Specky2.png";
+  const head = document.querySelector(".head");
+  const torso = document.querySelector(".torso");
+  const lower = document.querySelector(".lower");
 
-  setTimeout(() => {
-    img.src = "images/Specky1.png";
-    setTimeout(blink, Math.random() * 4000 + 2000);
-  }, 120);
-}
+  const messages = {
+    head: [
+      "You're looking for meaning.",
+      "Think a bit harder.",
+      "Why are you here?",
+      "",
+    ],
+    torso: [
+      "I'm hungry.",
+      "Don't poke me.",
+      "Hihihi, that tickles."
+    ],
+    lower: [
+      "Hey... buy me dinner first.",
+      "That's inappropriate!",
+      "Don't be a weirdo now.",
+      "Nope."
+    ]
+  };
 
-blink();
+  const index = {
+    head: 0,
+    torso: 0,
+    lower: 0
+  };
 
+  function setNextText(type) {
+    const lines = messages[type];
+    const i = index[type];
 
+    textBox.textContent = lines[i];
+    index[type] = (i + 1) % lines.length;
+  }
 
-const textBox = document.getElementById("dialogue");
+  head.onclick = () => setNextText("head");
+  torso.onclick = () => setNextText("torso");
+  lower.onclick = () => setNextText("lower");
 
-document.querySelector(".head").onclick = () => setNextText("head");
-document.querySelector(".torso").onclick = () => setNextText("torso");
-document.querySelector(".lower").onclick = () => setNextText("lower");
+  function blink() {
+    img.src = "images/Specky2.png";
 
-function setNextText(type) {
-  const lines = messages[type];  
-  const i = index[type];        
+    setTimeout(() => {
+      img.src = "images/Specky1.png";
+      setTimeout(blink, Math.random() * 4000 + 2000);
+    }, 120);
+  }
 
-  textBox.textContent = lines[i];
+  blink();
 
-  index[type] = (i + 1) % lines.length;
-}
-
-const messages = {
-
-  head: [
-    "You're looking for meaning.",
-    "Think a bit harder.",
-    "Why are you here?",
-    "",
-  ],
-
-  torso: [
-    "I'm hungry.",
-    "Don't poke me.",
-    "Hihihi, that tickles."
-  ],
-
-  lower: [
-    "Hey... buy me dinner first.",
-    "That's inappropriate!",
-    "Don't be a weirdo now.",
-    "Nope."
-  ]
-};
-
-const index = {
-  head: 0,
-  torso: 0,
-  lower: 0
 });
