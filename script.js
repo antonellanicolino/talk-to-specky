cf528f008ce766ca80b85a018f0bd3d7d447a8b2
antonellanicolino <antonellanicolino1999@gmail.com>
2026-05-11 20:33:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ blink();
 
 const textBox = document.getElementById("dialogue");
 
+textBox.textContent = "Click on Specky.";
+
 document.querySelector(".head").onclick = () => setNextText("head");
 document.querySelector(".torso").onclick = () => setNextText("torso");
 document.querySelector(".lower").onclick = () => setNextText("lower");
